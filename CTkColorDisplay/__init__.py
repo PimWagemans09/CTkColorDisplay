9291e4824b14960a0014400c9edd9fd8e292260b
Pim <58147793@leerling.agoramaasenpeel.nl>
2023-12-14 12:07:56 +0100

    added credits

--- CTkColorDisplay/__init__.py
+++ CTkColorDisplay/__init__.py
@@ -1,3 +1,5 @@
+#made by Pim Wagemans (github PimWagemans09)
+
 from .CTkColorDisplay import CTkColorDisplay
 from .utilities import verify_hex, hex_to_rgb, rgb_to_hex
 from .exceptions import invalidHexException
