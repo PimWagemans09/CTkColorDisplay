14b90fb356311a1de2d911409910085d94b11a3e
Pim <58147793@leerling.agoramaasenpeel.nl>
2023-12-14 12:05:58 +0100

    added ability to change the display_color on the fly and to trace a customtkinter.StringVar to set the color

--- CTkColorDisplay/CTkColorDisplay.py
+++ CTkColorDisplay/CTkColorDisplay.py
@@ -1,5 +1,6 @@
 from typing import Optional, Tuple, Union
 import customtkinter as ctk
+import tkinter
 from PIL import Image 
 import numpy as np
 from os.path import basename
@@ -7,7 +8,7 @@ from .utilities import hex_to_rgb, verify_hex
 from .exceptions import invalidHexException
 
 class CTkColorDisplay(ctk.CTkFrame):
-    def __init__(self, master: any, display_color:str = "#ffffff", display_image:str|Image.Image = __file__.replace(basename(__file__),"assets/colorsquare.png"), image_width: int = 32, image_height: int = 32, frame_width: int = 200, frame_height: int = 200, corner_radius: int | str | None = None, border_width: int | str | None = None, bg_color: str | Tuple[str, str] = "transparent", fg_color: str | Tuple[str, str] | None = "transparent", border_color: str | Tuple[str, str] | None = None, background_corner_colors: Tuple[str | Tuple[str, str]] | None = None, overwrite_preferred_drawing_method: str | None = None, **kwargs):
+    def __init__(self, master: any, display_color:str = "#ffffff", variable:ctk.StringVar = None,display_image:str|Image.Image = __file__.replace(basename(__file__),"assets/colorsquare.png"), image_width: int = 32, image_height: int = 32, frame_width: int = 200, frame_height: int = 200, corner_radius: int | str | None = None, border_width: int | str | None = None, bg_color: str | Tuple[str, str] = "transparent", fg_color: str | Tuple[str, str] | None = "transparent", border_color: str | Tuple[str, str] | None = None, background_corner_colors: Tuple[str | Tuple[str, str]] | None = None, overwrite_preferred_drawing_method: str | None = None, **kwargs):
         """
         a widget for displaying colors\n
         all defualt arguments of a frame stil apply\n
@@ -22,6 +23,8 @@ class CTkColorDisplay(ctk.CTkFrame):
         """
         super().__init__(master, frame_width, frame_height, corner_radius, border_width, bg_color, fg_color, border_color, background_corner_colors, overwrite_preferred_drawing_method, **kwargs)
         self._master = master
+        if variable != None:
+            variable.trace_add("write",callback=self._set_color_trace)
         self.configure(fg_color=fg_color)
         self.columnconfigure((0),weight=1)
         self.rowconfigure((0),weight=1)
@@ -32,11 +35,25 @@ class CTkColorDisplay(ctk.CTkFrame):
             self._original_image = Image.open(display_image)
         elif isinstance(display_image,Image.Image):
             self._original_image = display_image
-        self._original_image = self._original_image.resize((image_width,image_height))
+        self._color_display = ctk.CTkLabel(self,text="error",text_color="#ffffff",fg_color="#ff0000")
+        self._color_display.grid(row=0,column=0,sticky="nesw")
+        self._update_color_display()
+
+    def set_color(self, color:str):
+        self._display_color=color
+        if not verify_hex(self._display_color):
+            raise invalidHexException(f"{self._display_color} is not a valid hex color")
+        self._update_color_display()
+    
+    def _set_color_trace(self,*args):
+        color = self.winfo_toplevel().getvar(args[0])
+        self.set_color(color)
+
+    def _update_color_display(self):
+        self._color_display.destroy()
         self._colored_image = ctk.CTkImage(self._recolor_image(self._original_image,self._display_color))
         self._color_display = ctk.CTkLabel(self,text="",image=self._colored_image,fg_color="transparent")
         self._color_display.grid(row=0,column=0,sticky="nesw")
-    
         
     def _recolor_image(self,image:Image.Image,new_color:str,replace_color: str = "#ffffff") -> Image.Image:
         data = np.array(image)
