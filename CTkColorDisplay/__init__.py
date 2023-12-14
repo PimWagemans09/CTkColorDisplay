876999fe60c237ed9410959b3e9f8b53219aca78
Pim <58147793@leerling.agoramaasenpeel.nl>
2023-12-14 16:54:49 +0100

    added VERSION

--- CTkColorDisplay/__init__.py
+++ CTkColorDisplay/__init__.py
@@ -1,5 +1,7 @@
 #made by Pim Wagemans (github PimWagemans09)
 
+VERSION = 1.0
+
 from .CTkColorDisplay import CTkColorDisplay
 from .utilities import verify_hex, hex_to_rgb, rgb_to_hex
 from .exceptions import invalidHexException
